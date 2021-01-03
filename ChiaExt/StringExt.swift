1431bac8a0730f14b22693022e03af51cbba84d4
Ningv Chia <dozeuto@gmail.com>
2021-01-03 20:24:49 +0800

    Add string MD5 hash

--- ChiaExt/StringExt.swift
+++ ChiaExt/StringExt.swift
@@ -6,21 +6,23 @@
 //
 
 import Foundation
+import CryptoKit
 import CommonCrypto
 
 extension String {
     //  MARK:   MD5加密
-//    var md5: String {
-//        let data = Data(self.utf8)
-//        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-//            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-//            if #available(iOS 13.0, *) {
-//                CC_SHA256(bytes.baseAddress, CC_LONG(data.count), &hash)
-//            } else {
-//                CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
-//            }
-//            return hash
-//        }
-//        return hash.map { String(format: "%02x", $0) }.joined()
-//    }
+    var MD5: String {
+        let data = Data(self.utf8)
+        if #available(iOS 13.0, *) {
+            let computed = Insecure.MD5.hash(data: data)
+            return computed.map { String(format: "%02hhx", $0) }.joined()
+        } else {
+            let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
+                var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+                CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
+                return hash
+            }
+            return hash.map { String(format: "%02x", $0) }.joined()
+        }
+    }
 }
