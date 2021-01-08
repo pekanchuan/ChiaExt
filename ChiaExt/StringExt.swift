50be3216720325ac4f9242bc8861db81c6ee1805
Ningv Chia <dozeuto@gmail.com>
2021-01-08 09:30:01 +0800

    Add some extensions of UIColor & String
    UIColor add hexstring property, String add htmlAttributedString function

--- ChiaExt/StringExt.swift
+++ ChiaExt/StringExt.swift
@@ -5,7 +5,7 @@
 //  Created by Chia on 2021/1/3.
 //
 
-import Foundation
+import UIKit
 import CryptoKit
 import CommonCrypto
 
@@ -61,5 +61,31 @@ extension String {
             }
         }
         return false
+    }
+    
+    //  Parameterize HTML string
+    func htmlAttributedString(size: CGFloat = 24) -> NSAttributedString? {
+        let htmlTemplate = """
+        <!doctype html>
+        <html>
+          <head>
+            <style>
+              body {
+                font-family: -apple-system;
+                font-size: \(size)px;
+              }
+            </style>
+          </head>
+          <body>
+            \(self)
+          </body>
+        </html>
+        """
+        
+        guard let data = htmlTemplate.data(using: .utf8) else { return nil }
+        
+        guard let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) else { return nil }
+        
+        return attributedString
     }
 }
--- ChiaExt/UIColorExt.swift
+++ ChiaExt/UIColorExt.swift
@@ -34,4 +34,15 @@ extension UIColor {
         
         return nil
     }
+    
+    //  HEX string
+    var hexString: String? {
+        if let components = self.cgColor.components {
+            let r = components[0]
+            let g = components[1]
+            let b = components[2]
+            return String(format: "#%02x%02x%02x", (Int)(r * 255), (Int)(g * 255), (Int)(b * 255))
+        }
+        return nil
+    }
 }
