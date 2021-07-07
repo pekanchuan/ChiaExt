301387b21809d2d8b56af9730c22e2c76692d073
ChiaNingv <dozeuto@gmail.com>
2021-07-08 00:35:44 +0800

    remove duplicate items from an array

---
+++ ChiaExt/ArrayExt.swift
@@ -0,0 +1,22 @@
+//
+//  ArrayExt.swift
+//  ChiaExtDemo
+//
+//  Created by 贾发 on 2021/7/8.
+//
+
+import Foundation
+
+extension Array where Element: Hashable {
+    func removingDuplicates() -> [Element] {
+        var addedDict = [Element: Bool]()
+        
+        return filter {
+            addedDict.updateValue(true, forKey: $0) == nil
+        }
+    }
+    
+    mutating func removeDuplicates() {
+        self = self.removingDuplicates()
+    }
+}
