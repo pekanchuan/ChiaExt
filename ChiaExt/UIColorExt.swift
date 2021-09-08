0e13bf81f0c85595371b268448ab65ffd818f6e4
ChiaNingv <dozeuto@gmail.com>
2021-09-08 22:52:50 +0800

    Update UIColorExt

--- ChiaExt/UIColorExt.swift
+++ ChiaExt/UIColorExt.swift
@@ -7,42 +7,71 @@
 
 import UIKit
 
+// MARK: - Properties
+
 extension UIColor {
-    //  MARK:   HEX color
-    convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
+    
+    /// Random UIColor
+    static var random: UIColor {
+        let red = Int.random(in: 0...255)
+        let green = Int.random(in: 0...255)
+        let blue = Int.random(in: 0...255)
+        return UIColor(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1)
+    }
+}
+
+
+// MARK: - Initializers
+
+extension UIColor {
+    
+    /// Create UIColor from hexadecimal value with transparency
+    ///
+    /// - Parameters:
+    ///   - hex: hex Int (example: 0xEEEEEE)
+    ///   - transparency: transparency value (default is 1)
+    convenience init?(hex: Int, transparency: CGFloat = 1) {
+        var trans = transparency
+        if trans < 0 { trans = 0 }
+        if trans > 1 { trans = 1 }
         
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-            
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-                
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
-        }
-        
-        return nil
+        let red = (hex >> 16) & 0xFF
+        let green = (hex >> 8) & 0xFF
+        let blue = hex & 0xFF
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: trans)
     }
     
-    //  HEX string
-    var hexString: String? {
-        if let components = self.cgColor.components {
-            let r = components[0]
-            let g = components[1]
-            let b = components[2]
-            return String(format: "#%02x%02x%02x", (Int)(r * 255), (Int)(g * 255), (Int)(b * 255))
+    /// Create UIColor from hexadecimal string with transparency
+    ///
+    /// - Parameters:
+    ///   - hexString: hexadecimal string (examples: EDE7F6, 0xEDE7F6, #EDE7F6, #0ff, 0xF0F, ..)
+    ///   - transparency: transparency value (default is 1)
+    convenience init?(hexString: String, transparency: CGFloat = 1) {
+        var string = ""
+        let lowercasedHexString = hexString.lowercased()
+        if lowercasedHexString.hasPrefix("0x") {
+            string = lowercasedHexString.replacingOccurrences(of: "0x", with: "")
+        } else if hexString.hasPrefix("#") {
+            string = hexString.replacingOccurrences(of: "#", with: "")
+        } else {
+            string = hexString
         }
-        return nil
+        
+        if string.count == 3 {
+            var str = ""
+            string.forEach { str.append(String(repeating: String($0), count: 2))}
+            string = str
+        }
+        
+        guard let hexValue = Int(string, radix: 16) else { return nil }
+        
+        var trans = transparency
+        if trans < 0 { trans = 0 }
+        if trans > 1 { trans = 1 }
+        
+        let red = (hexValue >> 16) & 0xFF
+        let green = (hexValue >> 8) & 0xFF
+        let blue = hexValue & 0xFF
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: trans)
     }
 }
